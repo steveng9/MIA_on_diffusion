8f2ac3227ada79845319b32b6fb7303e6557a812
Steven Golob <golobs@uw.edu>
2025-05-17 16:09:39 -0700

    bugs

--- midst_models/single_table_TabDDPM/pipeline_utils.py
+++ midst_models/single_table_TabDDPM/pipeline_utils.py
@@ -572,6 +572,8 @@ def reconstruct_from_diffusion(
                 valid_mask = np.isin(x_cat_col, label_encoders[col].classes_)
                 print("num invalids: ", (1 - valid_mask).sum())
                 x_cat_col[~valid_mask] = most_common_value(x_cat_col)
+                valid_mask = np.isin(x_cat_col, label_encoders[col].classes_)
+                print("num invalids after fixing: ", (1 - valid_mask).sum())
                 encoded_x_cat.append(label_encoders[col].transform(x_cat_col))
                 print(len(x_cat_col))
                 raise e
